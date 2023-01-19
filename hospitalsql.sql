3bccf5a5abeabf083b4039a476af584c7f5f491e
Wendell Baresi <101334168+wendellbaresi@users.noreply.github.com>
2023-01-19 09:42:29 -0300

    Add files via upload

---
+++ hospitalsql.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE hospital;
+
+use hospital;
+
+describe paciente;
+
+describe medicacao;
+
+describe alta;
+
+select * from medicacao;
+
+select * from paciente;
+
+insert into paciente values(default,"02541536201","1993-06-10","garganta",5,"Wendell",3216540,2,2);
+
+insert into paciente values(default,"35715936925","1992-05-17","perna",2,"Julio",2318654,2,2);
+
+insert into paciente values(default,"02541536201","1993-06-10","garganta",5,"Wendell",3216540,2,2);
+
+select * from alta;
+
+truncate paciente;
+
+
+
+
